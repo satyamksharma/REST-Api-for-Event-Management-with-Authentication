004c9ebe6d607d86d52e206a89b39a7268331f0a
satyamksharma <ksatyam433@gmail.com>
2024-06-13 14:19:49 +0530

    cancel registration function done

--- routes/register.go
+++ routes/register.go
@@ -31,5 +31,22 @@ func registerForEvent(context *gin.Context) {
 }
 
 func cancelRegistration(context *gin.Context) {
+	userId := context.GetInt64("userId")
+	eventId, err := strconv.ParseInt(context.Param("id"), 10, 64)
+	
+
+	var event models.Event
+	event.ID = eventId
+
+	err = event.CancelRegistration(userId)
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"error": "Could not cancel registration"})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"message": "Successfully cancelled registration"})
+
+
 	
 }
