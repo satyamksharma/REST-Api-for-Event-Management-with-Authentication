cfc7c9706fd68be53d692b14cce7859175ed061d
satyamksharma <ksatyam433@gmail.com>
2024-06-13 10:28:05 +0530

    User schema created

--- routes/routes.go
+++ routes/routes.go
@@ -3,9 +3,14 @@ package routes
 import "github.com/gin-gonic/gin"
 
 func RegisterRoutes(server *gin.Engine) {
+	//Event Routes
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvents)
 	server.PUT("/events/:id", updateEvent)
 	server.DELETE("/events/:id", deleteEvent)
+
+	//User ROutes
+	server.POST("/signup", createUser)
+	// server.POST("/login", login)
 }
